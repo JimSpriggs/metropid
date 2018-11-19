7950c531f40a8e790bc28ea3f9c6d76e573d595d
john <jhurst1970@gmail.com>
2018-11-19 21:11:59 +0000

    Don't clear before writing first string

--- device/scrollphathd_display.py
+++ device/scrollphathd_display.py
@@ -7,10 +7,16 @@ class ScrollPHatHDDisplay(Display):
 
     def __init__(self, logger):
         super().__init__(logger)
+        self.message = None
 
     def set_message(self, message):
-        scrollphathd.clear()
+        if self.message is not None:
+            if self.message == message:
+                return
+            scrollphathd.clear()
+        print('Writing: '.format(message))
         scrollphathd.write_string(message, brightness=0.5)
+        self.message = message
 
     def update_display(self):
         scrollphathd.show()
